398fd7ce06b375ccb535e4ebc849eb8d28c87c42
andrewt319 <andrew319t@gmail.com>
2022-12-28 18:02:24 -0800

    Connecting to database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors';
+
+// setting up our app
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.chvkipp.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery',false);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+    
