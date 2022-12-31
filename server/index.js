388d5e07e3263b9585b1d5f6c196f1fd128aa638
andrewt319 <andrew319t@gmail.com>
2022-12-31 02:13:39 -0800

    Added changes for good practice

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import cors from 'cors';
 import dotenv from 'dotenv';
 
@@ -17,11 +18,10 @@ app.use(cors());
 // every route will start with /posts prefix
 app.use('/posts', postRoutes); // every route inside postRoutes will start from posts
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.chvkipp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery',false);
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
