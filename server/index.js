2159a573c7229e5527635f0c01e7d83e4e1e0058
andrewt319 <andrew319t@gmail.com>
2022-12-31 02:02:57 -0800

    Add some changes for good practice

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 // setting up our app
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
